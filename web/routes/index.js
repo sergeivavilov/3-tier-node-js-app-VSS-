05cc7a996e40edfff5dd3232362691fb0dfc45e4
sergeivavilov <157989606+sergeivavilov@users.noreply.github.com>
2024-05-20 21:14:05 -0600

    Update index.js

--- web/routes/index.js
+++ web/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-
-var api_url = process.env.API_HOST + '/api/status';
+var ipAddress = process.env.IP_ADDRESS || '10.7.110.213';  // Default IP address if not set in environment
+var api_url = 'http://' + ipAddress + '/api/status';
+// var api_url = process.env.VARIABLE + '/api/status';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
