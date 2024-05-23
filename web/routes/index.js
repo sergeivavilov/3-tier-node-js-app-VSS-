fc082ca5a36f121c1b8ef916b545373c28a175cf
sergeivavilov <157989606+sergeivavilov@users.noreply.github.com>
2024-05-22 23:09:04 -0600

    Update index.js

--- web/routes/index.js
+++ web/routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-
-var api_url = process.env.API_HOST + '/api/status';
+ var api_url = process.env.API_HOST + '/api/status';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
